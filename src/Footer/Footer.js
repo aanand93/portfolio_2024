52178053a5adc0273927e9e162115ad16e9aace7
Alexander Anand <77586218+aanand93@users.noreply.github.com>
2024-02-16 14:42:04 -0500

    Fix footer colors for links, fix link color for header, fix background color

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,10 +7,18 @@ const Footer = () => {
 		<footer className='footer'>
 			<hr className='footer-line' />
 			<div className='footer-links'>
-				<Link to='/'>Home</Link>
-				<Link to='/Projects'>Projects</Link>
-				<Link to='/Contact'>Contact</Link>
-				<Link to='/Resume'>Resume</Link>
+				<Link className='footer-link' to='/'>
+					Home
+				</Link>
+				<Link className='footer-link' to='/Projects'>
+					Projects
+				</Link>
+				<Link className='footer-link' to='/Contact'>
+					Contact
+				</Link>
+				<Link className='footer-link' to='/Resume'>
+					Resume
+				</Link>
 			</div>
 			<div className='foooter-icons'>
 				<a href='https://github.com/aanand93'>
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 	return (
-		<div className='navbar'>
-			<div className='Links'>
-				<Link to='/'>Home</Link>
-				<Link to='/Projects'>Projects</Link>
-				<Link to='/Contact'>Contact</Link>
-				<Link to='/Resume'>Resume</Link>
+		<nav className='navbar'>
+			<div className='navbar-toggle' onClick={toggleMenu}>
+				<div className={`menu-icon ${isOpen ? 'open' : ''}`}>
+					<div className='bar'></div>
+					<div className='bar'></div>
+					<div className='bar'></div>
+				</div>
 			</div>
-		</div>
+			<ul className={`Links ${isOpen ? 'open' : ''}`}>
+				<li>
+					<Link to='/' className='Link-individual'>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link to='/projects' className='Link-individual'>
+						Projects
+					</Link>
+				</li>
+				<li>
+					<Link to='/contact' className='Link-individual'>
+						Contact
+					</Link>
+				</li>
+				<li>
+					<Link to='/resume' className='Link-individual'>
+						Resume
+					</Link>
+				</li>
+			</ul>
+			{/* <div className='Links'>
+		  <Link className='Links' to='/'>
+			  Home
+		  </Link>
+		  <Link className='Links' to='/Projects'>
+			  Projects
+		  </Link>
+		  <Link className='Links' to='/Contact'>
+			  Contact
+		  </Link>
+		  <Link className='Links' to='/Resume'>
+		  Resume
+		  </Link>
+	  </div> */}
+		</nav>
 	);
 };
 
