361f450a984ccabfbc2586cad531ebf2ba85dc2e
Alexander Anand <77586218+aanand93@users.noreply.github.com>
2024-07-23 14:11:05 -0400

    update home page again

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,31 +10,26 @@ const Home = () => {
 				<img src={home} alt='Home' className='home-image' />
 				<div className='home-links home'>
 					<li>
-						I am a results-oriented Software Engineer with 3 years of
-						experience as a Frontend Developer with 10 years of experience in graphic
-						design and business logistics.
+						I am a results-oriented Software Engineer with 3 years of experience 
+						as a Frontend Developer and 10 years of experience in graphic design 
+						and business logistics. I am actively pursuing a full-time software 
+						engineering position with a focus on security and design.
 					</li>
 					<li>
-						I am actively pursuing a full-time software engineering position
-						with a focus on security and design.
-					</li>
-					<li>
-						I recently completed a bachelors degree in software development and
-						security, dedicating 18 months completing my degree and honing my expertise in
-						building efficient and secure web solutions.
-					</li>
-					<li>
-						I am an adept communicator with strong leadership skills, proficient
-						in collaborating with diverse teams, and thriving in dynamic
-						environments. I demonstrate composure and focus in high-pressure
-						scenarios, consistently achieving project objectives by leveraging
+						I recently completed a bachelor's degree in software development and security, 
+						dedicating 18 months to completing my degree and honing my expertise in building 
+						efficient and secure web solutions. I am an adept communicator with 
+						strong leadership skills, proficient in collaborating with diverse teams, 
+						and thriving in dynamic environments. I demonstrate composure and focus in 
+						high-pressure scenarios, consistently achieving project objectives by leveraging 
 						my technical expertise.
 					</li>
 					<li>
-						I am currently taking a course to obtain my security+ certification
-						as well as building a new webspace for young developers around the globe!
-						If you would like to learn more, please reach out!
+						Currently, I am taking a course to obtain my Security+ certification and building 
+						a new webspace for young developers around the globe! If you would like to learn 
+						more, please reach out!
 					</li>
+					
 				</div>
 			</div>
 		</div>
